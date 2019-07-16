4f6756366ddca7bf10d3964ad2f7c87829f63820
Eugene <jjjlexusjjj@rambler.ru>
2019-07-16 13:18:06 +0300

    extend auth

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,13 +1,25 @@
 import { Injectable } from '@angular/core';
-import {Observable, empty, from} from 'rxjs';
-import {User} from './user';
-import { tap, catchError } from 'rxjs/operators';
+import { Observable, empty, from } from 'rxjs';
+import { User } from './user';
+import { tap, catchError, map } from 'rxjs/operators';
 import { AngularFireAuth } from '@angular/fire/auth';
 
 @Injectable()
 export class AuthService {
 
   constructor(private afAuth: AngularFireAuth) { }
+
+  get currentUser() {
+    return this.afAuth.user
+      .pipe(map((u: firebase.User | null) => {
+          if (u) {
+            return new User()
+          } else {
+            return null;
+          }
+        }
+      ));
+  }
 
   public login(value: { username: string; password: string }): Observable<any> {
     return empty();
@@ -16,12 +28,12 @@ export class AuthService {
   public register(user: User, password: string): Observable<any> {
     console.log(`registering user ${user.email} with pass ${password}`, this.afAuth);
     return from(this.afAuth.auth.app.auth().createUserWithEmailAndPassword(user.email, password))
-    .pipe(
-      tap(r => console.log('registration complete', r)),
-      catchError(e => {
-        console.log('error on reg', e);
-        throw e;
-      })
-    );
+      .pipe(
+        tap(r => console.log('registration complete', r)),
+        catchError(e => {
+          console.log('error on reg', e);
+          throw e;
+        })
+      );
   }
 }
--- src/app/auth/user.ts
+++ src/app/auth/user.ts
@@ -2,4 +2,10 @@ export class User {
   id: number | string;
   email: string;
   name: string;
+
+  constructor(o?: {[P in keyof User]?: User[P]}) {
+    if (o) {
+      Object.assign(this, o);
+    }
+  }
 }
